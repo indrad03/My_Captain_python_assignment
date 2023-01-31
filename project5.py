fef20db40dc4cd1aa78ed2f5537dd78cef3cfe84
Darsi Indranath <122106011+indrad03@users.noreply.github.com>
2023-01-31 16:48:57 +0530

    Add files via upload

---
+++ project5.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 31 16:47:50 2023
+
+@author: INDRA
+"""
+
+def char_frequency(str1):
+    dict = {}
+    for n in str1:
+        keys = dict.keys()
+        if n in keys:
+            dict[n] += 1
+        else:
+            dict[n] = 1
+    return dict
+print(char_frequency('google.com'))
